dce171dbf43d6b71f6a7424369125caa92d172f4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-16 07:18:14 +0000

    Deploy man27382210/man27382210.github.io to github.com/man27382210/man27382210.github.io.git:gh-pages

---
+++ _next/static/chunks/pages/components/button-c81127f746fe9cf6ef32.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[7],{rHD1:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/button",function(){return o("kxSS")}])}},[["rHD1",0,1,2]]]);
